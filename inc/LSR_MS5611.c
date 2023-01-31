8eb645e274ed1c92d067cb65bcf8dde3a630cff3
roboticsmick <mick.venz@gmail.com>
2023-01-31 22:16:31 +1000

    comments

--- inc/LSR_MS5611.c
+++ inc/LSR_MS5611.c
@@ -48,11 +48,7 @@ bool ms5611_i2c_init(ms5611_data_t *ms5611, i2c_inst_t *i2c, float *sea_level_pr
         ms5611->ms5611_sensor_ready = MS5611_READY;
         ms5611->ms5611_adc_ready = MS5611_ADC_PENDING;
     }
-    // Print EEPROM Values
-    // for( i=0 ; i< MS5611_COEFF_NUMBERS ; i++)
-    // {
-    //     printf("%i.: %li\r\n",i,eeprom_coeff[i]);
-    // }
+
     return true;
 }
 
@@ -84,7 +80,7 @@ bool ms5611_adc_start(ms5611_data_t *ms5611) {
 
 bool ms5611_adc_read(ms5611_data_t *ms5611) {
     
-    // Set ADC data buffer
+    // Set data buffer vatiable
     uint8_t data[3];
 	data[0] = 0;
 	data[1] = 0;
@@ -100,7 +96,9 @@ bool ms5611_adc_read(ms5611_data_t *ms5611) {
     else {
         // Store ADC pressure value as uint32_t
         ms5611->pressure_adc  = ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | data[2];
+        // Calculate pressure, temp, and altitude
         ms5611_calc(ms5611);
+        // Set state to read temperature ADC
         ms5611->ms5611_sensor_read = READ_TEMPERATURE;
     }
     // Set sensor state to ready
@@ -112,15 +110,20 @@ bool ms5611_adc_read(ms5611_data_t *ms5611) {
 
 
 bool ms5611_calc(ms5611_data_t *ms5611) {
-
+    // Initialise variables
 	int32_t dT, TEMP;
 	int64_t OFF, SENS, PRESS, T2, OFF2, SENS2;
+
+    // Left shift << 8 multiplies the value by 2^8 (256 in decimal).
     dT = (int32_t)ms5611->temp_adc - ((int32_t)eeprom_coeff[MS5611_REF_TEMP_INDEX] <<8 );
+    
+    // Right shift >> 23 divides the value by 2^23 (8388608 in decimal)
     TEMP = 2000 + ((int64_t)dT * (int64_t)eeprom_coeff[MS5611_TEMP_COEFF_OF_TEMP_INDEX] >> 23) ;
 
     // Second order temperature compensation if temp below 20 degrees
     if( TEMP < 2000 )
     {
+        // Right shift >> 33 divides the value by 2^33 (8589934592 in decimal)
         T2 = ( 3 * ( (int64_t)dT * (int64_t)dT  ) ) >> 33;
         OFF2 = 61 * ((int64_t)TEMP - 2000) * ((int64_t)TEMP - 2000) / 16 ;
         SENS2 = 29 * ((int64_t)TEMP - 2000) * ((int64_t)TEMP - 2000) / 16 ;
@@ -134,88 +137,29 @@ bool ms5611_calc(ms5611_data_t *ms5611) {
     }
     else
     {
+        // Right shift >> 38 divides the value by 2^38 (274877906944 in decimal)
         T2 = ( 5 * ( (int64_t)dT  * (int64_t)dT  ) ) >> 38;
         OFF2 = 0 ;
         SENS2 = 0 ;
     }
 
     // OFF = OFF_T1 + TCO * dT
+    // Left shift << 16 multiplies the value by 2^16 (65536 in decimal).
     OFF = ( (int64_t)(eeprom_coeff[MS5611_PRESS_OFFSET_INDEX]) << 16 ) + ( ( (int64_t)(eeprom_coeff[MS5611_TEMP_COEFF_OF_PRESS_OFF_INDEX]) * dT ) >> 7 ) ;
     OFF -= OFF2 ;
 
     // Sensitivity at actual temperature = SENS_T1 + TCS * dT
+    // Left shift << 15 multiplies the value by 2^15 (32768 in decimal).
     SENS = ( (int64_t)eeprom_coeff[MS5611_PRESS_SENS_INDEX] << 15 ) + ( ((int64_t)eeprom_coeff[MS5611_TEMP_COEFF_OF_PRESS_SEN_INDEX] * dT) >> 8 ) ;
     SENS -= SENS2 ;
 
+    // Right shift >> 15 divides the value by 2^15 (32768 in decimal)
     PRESS = ( ( (ms5611->pressure_adc * SENS) >> 21 ) - OFF ) >> 15;
     ms5611->baro_temp_float = ((float)TEMP - T2 ) / 100;
     ms5611->pressure_float = (float)PRESS / 100;
     ms5611->alt_float = ((((pow((ms5611->sea_level_pressure/ms5611->pressure_float ), INV_GAMMA)) - 1) * (ms5611->baro_temp_float + CONVERT_C_TO_K))/TEMP_LAPSE_RATE);
     return true;
 }
-
-    // }
-    // // Buffer to store raw reads
-    // uint8_t data[6];
-    // // start conversion of the pressure sensor
-    // data[0] = 0x00;
-    // i2c_reg_write(MS5611_data->i2c_ptr, MS5611_ADDR, 0x44, &data[0], 1); // Pressure resolution RMS 0x44 = 1024 
-    // // read the pressure
-    // i2c_reg_read(MS5611_data->i2c_ptr, MS5611_ADDR, 0x00, data, 3);
-
-    // // extract the raw value
-    // adc_pressure  = ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | data[2];
-    
-    // sleep_ms(10);
-    // // start conversion of the Temperature sensor
-    // data[0] = 0x00;
-    // i2c_reg_write(MS5611_data.i2c_ptr, MS5611_ADDR, 0x54, &data[0], 1); // Temperature resolution RMS 0x54 = 1024 
-    // // Delay while conversion 
-    // sleep_ms(2);
-    // // read the temperature
-    // i2c_reg_read(MS5611_data.i2c_ptr, MS5611_ADDR, 0x00, data, 3);
-    // // extract the raw value
-    // sleep_ms(2);
-    // adc_temperature  = ((uint32_t)data[0] << 16) | ((uint32_t)data[1] << 8) | data[2];
-    // // convert the pressure reading
-
-    // dT = (int32_t)adc_temperature - ((int32_t)eeprom_coeff[MS5611_REFERENCE_TEMPERATURE_INDEX] <<8 );
-    // temp = 2000 + ((int64_t)dT * (int64_t)eeprom_coeff[MS5611_TEMP_COEFF_OF_TEMPERATURE_INDEX] >> 23) ;
-
-    // // Second order temperature compensation if temp below 20 degrees
-    // if( temp < 2000 )
-    // {
-    //     T2 = ( 3 * ( (int64_t)dT  * (int64_t)dT  ) ) >> 33;
-    //     OFF2 = 61 * ((int64_t)temp - 2000) * ((int64_t)temp - 2000) / 16 ;
-    //     SENS2 = 29 * ((int64_t)temp - 2000) * ((int64_t)temp - 2000) / 16 ;
-        
-    //     // Temperature compensation if temp below -15 degrees
-    //     if( temp < -1500 )
-    //     {
-    //         OFF2 += 17 * ((int64_t)temp + 1500) * ((int64_t)temp + 1500) ;
-    //         SENS2 += 9 * ((int64_t)temp + 1500) * ((int64_t)temp + 1500) ;
-    //     }
-    // }
-    // else
-    // {
-    //     T2 = ( 5 * ( (int64_t)dT  * (int64_t)dT  ) ) >> 38;
-    //     OFF2 = 0 ;
-    //     SENS2 = 0 ;
-    // }
-
-    // // OFF = OFF_T1 + TCO * dT
-    // OFF = ( (int64_t)(eeprom_coeff[MS5611_PRESSURE_OFFSET_INDEX]) << 16 ) + ( ( (int64_t)(eeprom_coeff[MS5611_TEMP_COEFF_OF_PRESSURE_OFFSET_INDEX]) * dT ) >> 7 ) ;
-    // OFF -= OFF2 ;
-
-    // // Sensitivity at actual temperature = SENS_T1 + TCS * dT
-    // SENS = ( (int64_t)eeprom_coeff[MS5611_PRESSURE_SENSITIVITY_INDEX] << 15 ) + ( ((int64_t)eeprom_coeff[MS5611_TEMP_COEFF_OF_PRESSURE_SENSITIVITY_INDEX] * dT) >> 8 ) ;
-    // SENS -= SENS2 ;
-
-    // press = ( ( (adc_pressure * SENS) >> 21 ) - OFF ) >> 15;
-    // MS5611_data.baro_temp_float = ((float)temp - T2 ) / 100;
-    // MS5611_data.pressure_float = (float)press / 100;
-    // MS5611_data.alt_float = ((((pow((MS5611_data->sea_level_pressure/MS5611_data->pressure_float ), INV_GAMMA)) - 1) * (MS5611_data->baro_temp_float + CONVERT_C_TO_K))/TEMP_LAPSE_RATE);
-    // printf("Function Pressure: %.2f | Temperature: %.2f | Altitude :%.2f\r\n", MS5611_data->pressure_float, MS5611_data->baro_temp_float, MS5611_data->alt_float);
 
 
 bool ms5611_filter(ms5611_data_t *ms5611) {
@@ -257,3 +201,13 @@ bool ms5611_crc_check(uint16_t *n_prom, uint8_t crc)
     return  ( n_rem == crc );
 }
 
+
+bool print_eeprom(void) {
+    // Initialise variables
+    uint8_t i;
+    // Print EEPROM Values
+    for( i=0 ; i< MS5611_COEFF_NUMBERS ; i++)
+    {
+        printf("%i.: %li\r\n",i,eeprom_coeff[i]);
+    }
+}
